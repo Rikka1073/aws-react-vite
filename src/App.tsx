98123b3bccaae9c6f190ede016a00abf6727eb5d
EC2 Default User <ec2-user@ip-172-31-35-199.ap-northeast-1.compute.internal>
2025-04-13 08:46:21 +0000

    テストのテキストの追加

--- src/App.tsx
+++ src/App.tsx
@@ -28,6 +28,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <p>テストです</p>
+      <p>表示さたら成功</p>
     </>
   )
 }
